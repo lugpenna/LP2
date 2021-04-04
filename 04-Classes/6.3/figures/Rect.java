ae331953ff81bb785ffc738ea6db52c031604bfd
lugpenna <lugpenna5@gmail.com>
2021-04-04 15:29:32 -0300

    Add files via upload

---
+++ 04-Classes/6.3/figures/Rect.java
@@ -0,0 +1,30 @@
+package figures;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+public class Rect extends Figure {
+
+    public Rect (int x, int y, int w, int h, Color contorno, Color fundo) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.contorno = contorno;
+        this.fundo = fundo;
+    }
+    private void print() {
+		System.out.format("Tamanho (%d,%d) / Posição (%d,%d)\n",
+		this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(this.contorno);
+        g2d.drawRect(this.x, this.y, this.w, this.h);
+        g2d.setColor(this.fundo);
+        g2d.fillRect(this.x, this.y, this.w, this.h);
+    }
+}
